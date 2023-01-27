4dca88a49a935dcafa7bd0b79f27967d82541361
MBAYE SENE <smbaye@ept.sn>
2023-01-27 14:31:41 +0000

    my first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './HomePage/components/home/home.component';
+import { NavbarComponent } from './HomePage/components/navbar/navbar.component';
+import { FooterComponent } from './HomePage/components/footer/footer.component';
+import { SliderComponent } from './HomePage/components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
